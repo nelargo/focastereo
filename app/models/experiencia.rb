396e4acf185c5fb249e9c3002e8fafa7bed0b2b2
Felipe Navarro Torres <felipe.navarrot@gmail.com>
2014-01-04 02:19:29 -0300

    borrado todo

--- app/models/experiencia.rb
+++
@@ -1,2 +0,0 @@
-class Experiencia < ActiveRecord::Base
-end
--- test/models/editar_curso_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EditarCursoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
