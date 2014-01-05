740a72c9ec7bdfd1b373f29b23ccc1b1b533eb69
Felipe Navarro Torres <felipe.navarrot@gmail.com>
2014-01-05 00:55:14 -0300

    una wea mula

--- app/controllers/alumno_controller.rb
+++ app/controllers/alumno_controller.rb
@@ -1,4 +1,24 @@
 class AlumnoController < ApplicationController
+  befor_filter :checkAlumno
+
   def index
+  	@user = session[:username]
+  	@etapa = Etapa.all
   end
+
+  def checkAlumno
+  	if session[:type] != "alumno"
+  		redirect_to root_path
+  		return
+  	else
+  		return
+  end
+
+  def postulacion
+  	@ramos = Ramo.find_by_activo(1)
+  end
+
+
+
+  
 end
