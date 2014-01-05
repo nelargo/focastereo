9e6e9d400e7b6ad6d7a51e382ce9a8b1892b77e6
Felipe Navarro Torres <felipe.navarrot@gmail.com>
2014-01-05 20:47:47 -0300

    sfdshg

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,8 @@ TSC2013::Application.routes.draw do
   post "admin/ramo_new"             => "admin#crear_ramo"
   get "admin/peticiones"            => "admin#peticiones"
   get "admin/ver_peticion/:id"      => "admin#ver_peticion",      :as => 'ver_peticion'
-  post "admin/aceptar_peticion/:id" => "admin#aceptar_peticion",  :as => 'aceptar_peticion'
-  post "admin/rechazar_peticion/:id"=> "admin#rechazar_peticion", :as => 'rechazar_peticion'  
+  get "admin/aceptar_peticion/:id" => "admin#aceptar_peticion",  :as => 'aceptar_peticion'
+  get "admin/rechazar_peticion/:id"=> "admin#rechazar_peticion", :as => 'rechazar_peticion'  
 
 #AUTHENTIFICATION
   get "auth/login"
