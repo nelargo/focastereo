7124b7da7b438643e1b41d21a34a126776ab8bf5
Felipe Navarro Torres <felipe.navarrot@gmail.com>
2014-01-06 03:04:19 -0300

    algo loco

---
+++ public/assets/tab.js
@@ -0,0 +1,8 @@
+<script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+<script src="http://code.jquery.com/ui/1.9.1/jquery-ui.js"></script>
+<script type="text/javascript">
+    $('#tabs')
+        .tabs()
+        .addClass('ui-tabs-vertical ui-helper-clearfix');
+
+  </script>
